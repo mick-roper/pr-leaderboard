9d4fa5dd51570f79a72032d7a73f1bbf7827a9c2
mick-roper <mick.roper@dazn.com>
2019-08-02 08:56:15 +0100

    added the entrypoint

--- app/main.go
+++ app/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+var port = flag.Int("port", 8080, "the port the server will listen on")
 
 func main() {
-	log.Println("hello, world")
+	flag.Parse()
+
+	r := mux.NewRouter()
+
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%v", *port),
+		Handler:      r,
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  10 * time.Second,
+	}
+
+	log.Fatal(server.ListenAndServe())
 }
