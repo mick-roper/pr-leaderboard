d1a79312bb4d301c9e7c897ebfd62c9b8980c40d
mick-roper <mick.roper@dazn.com>
2020-11-17 08:48:24 +0000

    added some more routes

--- api/main.go
+++ api/main.go
@@ -9,6 +9,9 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
+
+	"github.com/mick-roper/pr-leaderboard/api/routes"
 )
 
 var (
@@ -20,16 +23,25 @@ func main() {
 	flag.IntVar(&port, "port", 35005, "The port the server will listen on")
 	flag.Parse()
 
+	mux := http.NewServeMux()
+
+	routes.ConfigureGithubRoutes(mux)
+
 	server := http.Server{
-		Addr: fmt.Sprintf(":%v", port),
+		Addr:         fmt.Sprintf(":%v", port),
+		Handler:      mux,
+		WriteTimeout: 10 * time.Second,
+		ReadTimeout:  5 * time.Second,
 	}
 
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGQUIT)
 
 	go func() {
 		<-sigChan
+		log.Print("Server shutting down...")
 		server.Shutdown(context.Background())
 	}()
 
+	fmt.Print("Server listening at address", server.Addr)
 	log.Print(server.ListenAndServe())
 }
--- api/routes/github.go
+++ api/routes/github.go
@@ -2,7 +2,16 @@ package routes
 
 import "net/http"
 
-// ConfigureGithubRoutes for the server
-func ConfigureGithubRoutes(server *http.Server) {
+type (
+	githubHandler struct{}
+)
 
+// ConfigureGithubRoutes for the server
+func ConfigureGithubRoutes(mux *http.ServeMux) {
+	handler := githubHandler{}
+	mux.Handle("/github", &handler)
+}
+
+func (h *githubHandler) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	res.WriteHeader(200)
 }
